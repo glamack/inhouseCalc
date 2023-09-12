1102fa1c03dc36719d006dddfb9fca14b10eed0d
Colin Mack <colin.mack@glasgow.ac.uk>
2023-09-12 16:46:04 +0100

    first commit

---
+++ R/data package.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+
+educ_data <- read_csv("R/educ_data.csv")
+
---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Data as example
+#'
+#' Contaisn two variables
+#'
+#' @format A dataframe with two variables and 4 rows:
+#' \describe{
+#' \item{Education}{Education score}
+#' \item{Age}{Age of person}
+#' }
+#'
+#' @source {Created as example}
+#'
+#' @example
+#' data(educ_data)
+"educ_data"
