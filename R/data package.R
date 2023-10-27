c1f142539b6a063e715b805beab23f4fd03fe976
Colin Mack <colin.mack@glasgow.ac.uk>
2023-10-27 19:50:11 +0100

    listings data metadata

--- R/data package.R
+++ R/data package.R
@@ -1,4 +1,5 @@
 library(tidyverse)
 
 educ_data <- read_csv("data/educ_data.csv")
-
+listings <- read_csv("data/listings_edinburgh.csv")
+the_counted <- read_csv("data/the-counted-2016.csv")
--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Data as example
 #'
-#' Contaisn two variables
+#' Contains two variables
 #'
 #' @format A dataframe with two variables and 4 rows:
 #' \describe{
@@ -13,3 +13,27 @@
 #' @example
 #' data(educ_data)
 "educ_data"
+
+#' Data as example
+#'
+#' Contains two variables
+#'
+#' @format A dataframe with ten variables and 14014 rows:
+#' \describe{
+#' \item{name}{name}
+#' \item{neighbourhood_cleansed}{neighbourhood}
+#' \item{neighbourhood_cleansed}{neighbourhood}
+#' \item{room_type}{room type}
+#' \item{accommodates}{number accommodated}
+#' \item{bathrooms}{bathrooms}
+#' \item{bedrooms}{bedrooms}
+#' \item{beds}{beds}
+#' \item{price}{price}
+#' \item{review_scores_rating}{reviews}
+#' }
+#'
+#' @source {Created as example}
+#'
+#' @example
+#' data(listings)
+"listings"
